d9493c95e4c148ff50d6ec90228542bd6ee43fac
Sezgin Öztürk <szgnoztrk@gmail.com>
2021-09-21 14:33:34 +0300

    CreditApplicationServiceApi is ready.

--- CreditApplicationServiceAPI/src/main/java/dev/patika/app/utility/config/SpringFoxConfig.java
+++ CreditApplicationServiceAPI/src/main/java/dev/patika/app/utility/config/SpringFoxConfig.java
@@ -1,4 +1,4 @@
-package dev.patika.app.config;
+package dev.patika.app.utility.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
