db7ed3ee39cad3aacaad0e9b2ead9ceead14066c
Sezgin Öztürk <szgnoztrk@gmail.com>
2021-09-22 11:45:52 +0300

    apply-service uploaded.

--- apply-service/src/main/java/dev/patika/applyservice/controller/ApplyController.java
+++ apply-service/src/main/java/dev/patika/applyservice/controller/ApplyController.java
@@ -1,6 +1,10 @@
 package dev.patika.applyservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,7 +17,7 @@ public class ApplyController {
     @GetMapping
     public void save(@RequestParam String ssid) {
         /* TODO - Check customer with SSID */
-        ResponseEntity responseEntity = this.template.getForEntity();
+        //ResponseEntity responseEntity = this.template.getForEntity();
         //Customer customer = (Customer) responseEntity.getBody();
 
 
