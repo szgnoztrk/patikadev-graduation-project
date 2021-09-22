ebc19dc61f4700609a91052b1a35375e71ce9c7f
Sezgin Öztürk <szgnoztrk@gmail.com>
2021-09-22 14:38:22 +0300

    ConfigServerand EurekaServer uploaded.

--- apply-service/src/main/java/dev/patika/applyservice/ApplyServiceApplication.java
+++ apply-service/src/main/java/dev/patika/applyservice/ApplyServiceApplication.java
@@ -2,8 +2,10 @@ package dev.patika.applyservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableEurekaClient
 @SpringBootApplication
 @EnableSwagger2
 public class ApplyServiceApplication {
